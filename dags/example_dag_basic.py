3561aecad61997d5395f33b57e12c088bafc46ef
frankbreetz <frank.breetz@gmail.com>
2022-11-22 16:36:25 -0500

    update dag file

--- dags/example_dag_basic.py
+++ dags/example_dag_basic.py
@@ -8,7 +8,6 @@ from airflow.decorators import dag, task # DAG and task decorators for interfaci
 #another comment
 # a third comment
 # a fourth comment
-# a fifth comment
 @dag(
     # This defines how often your DAG will run, or the schedule by which your DAG runs. In this case, this DAG
     # will run daily
